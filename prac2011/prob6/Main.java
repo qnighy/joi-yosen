63389980483411f80c48b1b62c00e5bddb96e096
Masaki Hara <ackie.h.gmai@gmail.com>
2010-12-19 10:46:17 +0900

    prac2011 prob6

---
+++ prac2011/prob6/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.Math.*;
+import static java.util.Arrays.*;
+
+public class Main {
+  static Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    int n = sc.nextInt();
+    for(int i = 0; i < n; i++) {
+      String s = sc.next();
+      System.out.println(s.toLowerCase());
+    }
+  }
+}
+

0906b6b7564ff83e50fd42c68346fa6f97d2eccd
Masaki Hara <ackie.h.gmai@gmail.com>
2010-12-19 10:10:59 +0900

    prac 2011 prob1

---
+++ prac2011/prob1/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.Math.*;
+import static java.util.Arrays.*;
+
+public class Main {
+  static Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println((a+b)+" "+(a-b));
+  }
+}
+

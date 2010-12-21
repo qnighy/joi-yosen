47d4c24467c2de25353fb06af77f74b84478a5af
Masaki Hara <ackie.h.gmai@gmail.com>
2010-12-21 18:20:25 +0900

    yosen2011-java prob6 fastened

--- yosen2011-java/prob6/Main.java
+++ yosen2011-java/prob6/Main.java
@@ -16,16 +16,11 @@ public class Main {
     String[] snip = new String[m];
     for(int i = 0; i < m; i++) snip[i] = sc.next();
     int wholecount = 1;
-    int[][] dp = new int[3][1<<n];
+    int[][] dp = new int[3][1<<(n-1)];
     dp[0][0] = 1;
     for(int y = 0; y < m; y++) {
       for(int x = 0; x < n; x++) {
-        /* for(int r = 0; r < 3; r++) {
-          for(int p = 0; p < (1<<n); p++) {
-            if(dp[r][p]!=0)System.out.println("("+y+","+x+") ("+numchar[r]+","+p+") : "+dp[r][p]);
-          }
-        } */
-        int[][] dp2 = new int[3][1<<n];
+        int[][] dp2 = new int[3][1<<(n-1)];
         char cur = snip[y].charAt(x);
         if(cur=='?') {
           wholecount=wholecount*3%MOD;
@@ -33,7 +28,13 @@ public class Main {
         for(int c = 0; c < 3; c++) {
           if(cur!='?' && cur!=numchar[c]) continue;
           for(int r = 0; r < 3; r++) {
-            for(int p = 0; p < (1<<n); p++) {
+            for(int p = 0; p < (1<<(n-1)); p++) {
+              while(true) {
+                int aa = p&(p>>1);
+                if(aa==0) break;
+                p += aa;
+              }
+              if(p >= (1<<(n-1))) break;
               int np = p>>1;
               if(r==2 && c==1 && 0<x) np |= 1<<(n-2);
               if((p&1)==1 && c==0) continue;
@@ -46,9 +47,8 @@ public class Main {
     }
     int sum = wholecount;
     for(int r = 0; r < 3; r++) {
-      for(int p = 0; p < (1<<n); p++) {
+      for(int p = 0; p < (1<<(n-1)); p++) {
         sum = (sum+MOD-dp[r][p])%MOD;
-        // if(dp[r][p]!=0)System.out.println("("+numchar[r]+","+p+") : "+dp[r][p]);
       }
     }
     System.out.println(sum);

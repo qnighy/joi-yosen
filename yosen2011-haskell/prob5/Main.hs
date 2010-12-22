0ac8a82b4db9ccdc141203693d19ebd769e730b4
Masaki Hara <ackie.h.gmai@gmail.com>
2010-12-22 12:41:17 +0900

    yosen2011-haskell prob5 Map -> Set (still lazy, about 350 secs amount)

--- yosen2011-haskell/prob5/Main.hs
+++ yosen2011-haskell/prob5/Main.hs
@@ -4,6 +4,9 @@ import Data.Maybe
 import qualified Data.Array as Array
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Debug.Trace
 
 numchar :: [Char]
 numchar = "S123456789"
@@ -15,45 +18,47 @@ main = do
         -- putStrLn $ show $ [h,w,n]
         -- putStrLn $ show $ m
         -- putStrLn $ show $ findPoint m 0
+        -- putStrLn $ show $ calc2 m (h,w) (findPoint m 0) (findPoint m 1)
         putStrLn $ show $ calc m (h,w) n
 
 calc :: [String] -> (Int,Int) -> Int -> Int
 calc m (h,w) n = foldl (+) 0 (map (\x->
-        (calc2 m (h,w) (findPoint m x) (findPoint m (x+1)))
+        (("stage "++show x) `trace` calc2 m (h,w) (findPoint m x) (findPoint m (x+1)))
         ) (Array.range (0,n-1)))
 
 calc2 :: [String] -> (Int,Int) -> (Int,Int) -> (Int,Int) -> Int
-calc2 m b s g =
+calc2 m (h,w) s g =
         (search
-        (rectMap b (\p->m!!fst p!!snd p))
-        b [s] [] 0 (rectMap b (\p->if p==s then 0 else -1) )) Map.! g
+        (Set.fromList [(y,x) | (y,x) <- Array.range ((0,0),(h-1,w-1)), m!!y!!x/='X', (y,x)/=s])
+        [s] [] 0 g )
 
-search :: Map (Int,Int) Char -> (Int,Int) -> [(Int,Int)] -> [(Int,Int)] -> Int -> Map (Int,Int) Int -> Map (Int,Int) Int
-search m (h,w) [] [] ct tim = tim
-search m (h,w) [] q ct tim = search m (h,w) q [] (ct+1) tim
-search m (h,w) ((y,x):q0) q1 ct tim =
-        let nei = validNeighbors m (h,w) (y,x) in
-        let nei' = filter (\p->(tim Map.! p) == -1) nei in
-        search m (h,w) q0 (nei'++q1) ct $! (foldl (\z p->Map.insert p (ct+1) z) tim nei')
-
-validNeighbors :: Map (Int,Int) Char -> (Int,Int) -> (Int,Int) -> [(Int,Int)]
-validNeighbors m b (y,x) =
-        filter (isValidPoint m b) [(y,x-1),(y,x+1),(y-1,x),(y+1,x)]
-
-isValidPoint :: Map (Int,Int) Char -> (Int,Int) -> (Int,Int) -> Bool
-isValidPoint m (h,w) (y,x) =
-        0<=y && 0<=x && y<h && x<w && ((m Map.! (y,x)) /= 'X')
+search :: Set (Int,Int) -> [(Int,Int)] -> [(Int,Int)] -> Int -> (Int,Int) -> Int
+search r [] [] ct g = -1
+search r [] q ct g = search r q [] (ct+1) g
+search r ((y,x):q0) q1 ct g =
+        -- r `seq`
+        -- y `seq`
+        -- x `seq`
+        -- q0 `seq`
+        -- q1 `seq`
+        -- ct `seq`
+        -- g `seq`
+        -- (show (y,x,length q0,length q1,ct)) `trace`
+        if (y,x) == g then
+          ct
+        else
+          let nei = filter (`Set.member` r) [(y,x-1),(y,x+1),(y-1,x),(y+1,x)] in
+          -- nei `seq`
+          let rr = (foldl (\z w->Set.delete w z) r nei) in
+          -- rr `seq`
+          -- (nei++q1) `seq`
+          search rr q0 (nei++q1) ct g
 
 findPoint :: [String] -> Int -> (Int,Int)
 findPoint m i =
         let m0 = map (elemIndex (numchar!!i)) m in
         let y = fromMaybe 0 (findIndex isJust m0) in
         let x = fromMaybe 0 (m0!!y) in (y,x)
-
-rectMap :: (Int,Int) -> ((Int,Int) -> a) -> Map (Int,Int) a
-rectMap (w,h) f =
-        let r = ((0,0),(w-1,h-1)) in
-        foldl (\m p -> Map.insert p (f p) m) Map.empty (Array.range r)
 
 (!!?) :: [a] -> Int -> Maybe a
 [] !!? _ = Nothing

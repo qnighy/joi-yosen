b3f289c624d92f0e40c9723fa0c3d816987f2f4c
Masaki Hara <ackie.h.gmai@gmail.com>
2010-12-25 22:06:45 +0900

    yosen2011-haskell prob5 using STUArray

--- yosen2011-haskell/prob5/Main.hs
+++ yosen2011-haskell/prob5/Main.hs
@@ -1,11 +1,10 @@
 import Control.Monad
+import Control.Monad.ST
 import Data.List
 import Data.Maybe
-import qualified Data.Array as Array
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
+import Data.Array.ST
+import Data.Array.MArray
+import Data.STRef
 import Debug.Trace
 
 numchar :: [Char]
@@ -15,53 +14,56 @@ main :: IO ()
 main = do
         [h,w,n] <- (getLine >>= return.map read.words)::IO [Int]
         m <- replicateM h (getLine >>= return.head.words)
-        -- putStrLn $ show $ [h,w,n]
-        -- putStrLn $ show $ m
-        -- putStrLn $ show $ findPoint m 0
-        -- putStrLn $ show $ calc2 m (h,w) (findPoint m 0) (findPoint m 1)
-        putStrLn $ show $ calc m (h,w) n
+        putStrLn $ show $ calc (concat m) (h,w) n
 
-calc :: [String] -> (Int,Int) -> Int -> Int
-calc m (h,w) n = foldl (+) 0 (map (\x->
-        (("stage "++show x) `trace` calc2 m (h,w) (findPoint m x) (findPoint m (x+1)))
-        ) (Array.range (0,n-1)))
+calc :: String -> (Int,Int) -> Int -> Int
+calc m dim n = foldl (+) 0 $ map (\x->calc2 m dim
+        (fromMaybe 0 $ elemIndex (numchar!!x) m)
+        (fromMaybe 0 $ elemIndex (numchar!!(x+1)) m)
+        ) [0..n-1]
 
-calc2 :: [String] -> (Int,Int) -> (Int,Int) -> (Int,Int) -> Int
-calc2 m (h,w) s g =
-        (search
-        (Set.fromList [(y,x) | (y,x) <- Array.range ((0,0),(h-1,w-1)), m!!y!!x/='X', (y,x)/=s])
-        [s] [] 0 g )
+calc2 :: String -> (Int,Int) -> Int -> Int -> Int
+calc2 m (w,h) s g = runST $ do
+        qsR <- newSTRef 0
+        qeR <- newSTRef 1
+        q <- (newArray (0,w*h) 0)::ST s (STUArray s Int Int)
+        tim <- newArray (0,w*h) (-1)
+        writeArray q 0 s
+        writeArray tim s 0
+        bfs m (w,h) q qsR qeR tim
+        readArray tim g
 
-search :: Set (Int,Int) -> [(Int,Int)] -> [(Int,Int)] -> Int -> (Int,Int) -> Int
-search r [] [] ct g = -1
-search r [] q ct g = search r q [] (ct+1) g
-search r ((y,x):q0) q1 ct g =
-        -- r `seq`
-        -- y `seq`
-        -- x `seq`
-        -- q0 `seq`
-        -- q1 `seq`
-        -- ct `seq`
-        -- g `seq`
-        -- (show (y,x,length q0,length q1,ct)) `trace`
-        if (y,x) == g then
-          ct
-        else
-          let nei = filter (`Set.member` r) [(y,x-1),(y,x+1),(y-1,x),(y+1,x)] in
-          -- nei `seq`
-          let rr = (foldl (\z w->Set.delete w z) r nei) in
-          -- rr `seq`
-          -- (nei++q1) `seq`
-          search rr q0 (nei++q1) ct g
+bfs :: String -> (Int,Int) -> STUArray s Int Int -> STRef s Int -> STRef s Int -> STUArray s Int Int -> ST s ()
+bfs m dim q qsR qeR tim = do
+        qs <- readSTRef qsR
+        qe <- readSTRef qeR
+        if qs==qe then return () else do
+          cur <- readArray q qs
+          curtim <- readArray tim cur
+          modifySTRef qsR (+1)
+          forM_ (neighbors dim cur) (\nei-> do
+            t <- readArray tim nei
+            if t>=0 || m!!nei=='X' then return () else do
+              qe' <- readSTRef qeR
+              writeArray q qe' nei
+              modifySTRef qeR (+1)
+              writeArray tim nei (curtim+1)
+            )
+          bfs m dim q qsR qeR tim
 
-findPoint :: [String] -> Int -> (Int,Int)
-findPoint m i =
-        let m0 = map (elemIndex (numchar!!i)) m in
-        let y = fromMaybe 0 (findIndex isJust m0) in
-        let x = fromMaybe 0 (m0!!y) in (y,x)
+neighbors :: (Int,Int) -> Int -> [Int]
+neighbors dim p =
+        let (y,x) = posToXY dim p in
+        mapMaybe (xyToPos dim) [(y,x-1),(y,x+1),(y-1,x),(y+1,x)]
 
-(!!?) :: [a] -> Int -> Maybe a
-[] !!? _ = Nothing
-(x:xs) !!? 0 = Just x
-(x:xs) !!? i = xs !!? (i-1)
+posToXY :: (Int,Int) -> Int -> (Int,Int)
+posToXY (h,w) p = divMod p w
+
+xyToPos :: (Int,Int) -> (Int,Int) -> Maybe Int
+xyToPos (h,w) (y,x) = do
+        guard $ 0<=x
+        guard $ 0<=y
+        guard $ x<w
+        guard $ y<h
+        return (y*w+x)
 
